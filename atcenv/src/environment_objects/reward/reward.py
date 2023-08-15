af18adc83ddb61fe738423f3bd3777b5deca687d
jangroter <47716241+jangroter@users.noreply.github.com>
2023-08-15 14:01:24 +0200

    added base class for the reward handling

---
+++ atcenv/src/environment_objects/reward/reward.py
@@ -0,0 +1,21 @@
+from shapely.geometry import Point, Polygon
+from typing import Tuple, Optional, List
+import math
+import random
+import numpy as np
+import pickle
+
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+import atcenv.src.functions as fn
+from atcenv.src.environment_objects.flight import Flight
+
+class Reward(ABC):
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_reward(self, flights: List[Flight]) -> np.ndarray:
+        pass

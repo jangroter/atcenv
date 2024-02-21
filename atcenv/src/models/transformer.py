bc63683c206ce6ee45c3ef7cd85af1d72c029e4b
jangroter <47716241+jangroter@users.noreply.github.com>
2024-02-21 16:17:33 +0100

    Added additive attention & multi head eq.

--- atcenv/src/models/transformer.py
+++ atcenv/src/models/transformer.py
@@ -5,6 +5,103 @@ from torch.distributions import Normal
 
 from typing import Tuple
 
+"""
+for NN attention (not implemented) maybe I can do:
+keys_unfold = keys.view(b,t,t-1,h,in_dim)
+queries_unfold = queries.view(b,t,h,in_dim)
+queries_unfold = queries.view(b,t,1,h,in_dim)
+
+kq = keys_unfold + queries_unfold
+
+# kq.size() = (b,t,t-1,h,in_dim)
+
+custom_weight_matrix = torch.nn.Parameter(torch.rand(h,in_dim,out_dim).uniform_(-0.1,0.1))
+
+
+# value / context vector as calculated from each head
+v_kq = torch.matmul(kq.transpose(2,3),custom_weight_matrix.transpose(1,2))
+
+# v_kq.size() = (b,t,h,t-1,out_dim)
+
+# then rearrange to get shape (b,t,t-1,h,out_dim)
+
+"""
+
+class RelativeAdditiveAttention(nn.Module):
+
+    def __init__(self, q_dim, kv_dim):
+        super(RelativeAdditiveAttention, self).__init__()
+
+        # These compute the queries, keys and values for all 
+        # heads (as a single concatenated vector)
+        # Generate the transformer matrices
+        self.tokeys    = nn.Linear(kv_dim, kv_dim, bias=False)
+        self.toqueries = nn.Linear(q_dim, kv_dim, bias=False)
+        self.tovalues  = nn.Linear(kv_dim, kv_dim, bias=False)
+
+        self.bias = nn.Parameter(torch.rand(kv_dim).uniform_(-0.1, 0.1))
+        self.score_proj = nn.Linear(kv_dim, 1)
+    
+    def forward(self, q_x, kv_x):
+        b, t,_, k = kv_x.size()
+        
+        keys = self.tokeys(kv_x).view(b,t,(t-1),k)
+        queries =  self.toqueries(q_x).view(b,t,1,k)
+
+        score = self.score_proj(torch.tanh(keys + queries + self.bias)).squeeze(-1)
+        
+        values = self.tovalues(kv_x)
+        
+        w = F.softmax(score, dim=-1)
+
+        x = torch.matmul(w.view(b,t,1,t-1),values)
+        x = x.view(b,t,k)
+        return x
+
+class RelativeAdditiveAttentionMultiHead(nn.Module):
+    """
+    for how multihead additive
+    **Attention-Based Models for Speech Recognition**: https://arxiv.org/abs/1506.07503
+    """
+
+    def __init__(self, q_dim, kv_dim, num_heads=3):
+        super(RelativeAdditiveAttention, self).__init__()
+
+        # These compute the queries, keys and values for all 
+        # heads (as a single concatenated vector)
+        # Generate the transformer matrices
+        self.num_heads = num_heads
+
+        self.tokeys    = nn.Linear(kv_dim, kv_dim*num_heads, bias=False)
+        self.toqueries = nn.Linear(q_dim, kv_dim*num_heads, bias=False)
+        self.tovalues  = nn.Linear(kv_dim, kv_dim*num_heads, bias=False)
+
+        self.bias = nn.Parameter(torch.rand(kv_dim).uniform_(-0.1, 0.1))
+        self.score_proj = nn.Linear(kv_dim, 1)
+    
+    def forward(self, q_x, kv_x):
+        b, t,_, k = kv_x.size()
+        h = self.num_heads
+        
+        keys = self.tokeys(kv_x).view(b,t,(t-1),h,k)
+        queries =  self.toqueries(q_x).view(b,t,h,k)
+        queries = queries.view(b,t,1,h,k)
+
+        score = self.score_proj(torch.tanh(keys + queries + self.bias)).squeeze(-1)
+        
+        values = self.tovalues(kv_x)
+        values = values.view(b,t,t-1,h,k)
+        
+        w = F.softmax(score, dim=2)
+
+        w = w.transpose(2,3)
+        v = values.transpose(2,3).transpose(3,4)
+
+        x = torch.matmul(w,v)
+        x = x.view(b,t,k*h)
+        return x
+
+
 class RelativeMultiHeadAttention(nn.Module):
     """
     Wide mult-head self-attention layer.
@@ -130,8 +227,8 @@ class MultiHeadAttention(nn.Module):
 
         return y
 
-class OwnRefTransformerBlock(nn.Module):
-    def __init__(self, q_dim, kv_dim, num_heads):
+class OwnRefTransformerBlockAdditive(nn.Module):
+    def __init__(self, q_dim, kv_dim):
         """
         Basic transformer block.
 
@@ -140,18 +237,18 @@ class OwnRefTransformerBlock(nn.Module):
             heads: number of heads (k mod heads must be 0)
 
         """
-        super(OwnRefTransformerBlock, self).__init__()
+        super(OwnRefTransformerBlockAdditive, self).__init__()
 
-        self.att = RelativeMultiHeadAttention(q_dim, kv_dim, num_heads=num_heads)
+        self.att = RelativeAdditiveAttention(q_dim, kv_dim)
 
-        self.norm1 = nn.LayerNorm(kv_dim * num_heads)
+        self.norm1 = nn.LayerNorm(kv_dim)
 
         self.ff = nn.Sequential(
-            nn.Linear(kv_dim * num_heads, 4 * kv_dim * num_heads),
+            nn.Linear(kv_dim, 5 * kv_dim),
             nn.ReLU(),
-            nn.Linear(4 * kv_dim * num_heads, kv_dim * num_heads))
+            nn.Linear(5 * kv_dim, kv_dim))
         
-        self.norm2 = nn.LayerNorm(kv_dim * num_heads)
+        self.norm2 = nn.LayerNorm(kv_dim)
 
     def forward(self, state):
         """
@@ -287,4 +384,97 @@ class TransformerBlock(nn.Module):
 
         return y
     
+class OwnRefTransformerBlock(nn.Module):
+    def __init__(self, q_dim, kv_dim, num_heads):
+        """
+        Basic transformer block.
 
+        Args:
+            k: embedding dimension
+            heads: number of heads (k mod heads must be 0)
+
+        """
+        super(OwnRefTransformerBlock, self).__init__()
+
+        self.att = RelativeMultiHeadAttention(q_dim, kv_dim, num_heads=num_heads)
+
+        self.norm1 = nn.LayerNorm(kv_dim * num_heads)
+
+        self.ff = nn.Sequential(
+            nn.Linear(kv_dim * num_heads, 5 * kv_dim * num_heads),
+            nn.ReLU(),
+            nn.Linear(5 * kv_dim * num_heads, kv_dim * num_heads))
+        
+        self.norm2 = nn.LayerNorm(kv_dim * num_heads)
+
+    def forward(self, state):
+        """
+        Forward pass of trasformer block.
+
+        Args:
+            x: input with shape of (b, k)
+        
+        Returns:
+            y: output with shape of (b, k)
+        """
+
+        q_x, kv_x = self.get_input_tensors(state)
+
+        # Self-attend
+        y = self.att(q_x,kv_x)
+
+        # First residual connection
+        # x = q_x + y
+        x = y
+
+        # Normalize
+        x = self.norm1(x)
+
+        # Pass through feed-forward network
+        y = self.ff(x)
+
+        # Second residual connection
+        x = x + y
+
+        # Again normalize
+        y = self.norm2(x)
+
+        return y
+    
+    def get_input_tensors(self, state):
+        rel_state_init = state[:,:,0:4]
+
+        b, t, k = rel_state_init.size()
+        
+        # Subtracts this vector from itself to generate a relative state matrix 
+        rel_state = rel_state_init.view(b,1,t,k) - rel_state_init.view(b,1,t,k).transpose(1,2)
+        
+        # Create x,y tensors for each aircraft by doubling the k dimension
+        xy_state = rel_state.view(b,t,t,2,2).transpose(3,4)
+        angle = state[:,:,-1]
+        angle = -angle + 0.5 * torch.pi
+        angle = angle.view(b,t,1)
+        x_new = xy_state[:,:,:,0,:] * torch.cos(angle.view(b,1,t,1)) - xy_state[:,:,:,1,:]*torch.sin(angle.view(b,1,t,1))
+        y_new = xy_state[:,:,:,0,:] * torch.sin(angle.view(b,1,t,1)) + xy_state[:,:,:,1,:]*torch.cos(angle.view(b,1,t,1))
+        rel_state = torch.cat((x_new,y_new),dim=3).view(b,t,t,2,2).transpose(3,4).reshape(b,t,t,4)
+
+        # Create a boolean mask to exclude the diagonal elements
+        mask = ~torch.eye(t, dtype=bool).unsqueeze(0).repeat(b,1,1)
+        rel_state = rel_state[mask].view(b, t, t-1, k)
+
+        # Calculate absolute distance matrix of size (batch,n_agents,n_agents-1,1)
+        r = torch.sqrt(rel_state[:,:,:,0].clone()**2 + rel_state[:,:,:,1].clone()**2).view(b,t,t-1,1)
+        
+        # Apply transformation to distance to have a distance closer to zero have a higher value, assymptotically going to zero.
+        r_trans = (1/(1+torch.exp(-1+5.*(r-0.2))))
+
+        # divide x and y by the distance to get the values as a function of the unit circle
+        rel_state[:,:,:,0:2] = rel_state[:,:,:,0:2].clone()/r
+        
+        # add transformed distance vector to the state vector
+        rel_state = torch.cat((rel_state,r_trans),dim=-1)
+
+        q_x = state
+        kv_x = rel_state
+
+        return q_x, kv_x
